8e9cf69bc2ebef10240367c96c7f0aab7a539cf6
erictc95 <maxerictc95@hotmail.com>
2026-03-24 11:24:33 +0100

    perf - fixed.

--- src/main/java/level3/exercises/records/PersonClass.java
+++ src/main/java/level3/exercises/records/PersonClass.java
@@ -45,7 +45,7 @@ public class PersonClass {
     }
 
 
-    /*
+    /* Compara un record amb una classe tradicional i comenta les diferències en llegibilitat i utilitat.
     La diferència entre un record i una classe tradicional és que el record genera automàticament el constructor,
     els getters, toString(), equals() i hashCode(), mentre que en una classe tradicional s’han d’implementar manualment.
     El record és immutable per defecte, cosa que el fa més segur per a models de dades simples.
--- src/main/java/level3/exercises/records/PersonMain.java
+++ src/main/java/level3/exercises/records/PersonMain.java
@@ -1,8 +1,5 @@
 package level3.exercises.records;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class PersonMain {
     public static void main(String[] args) {
